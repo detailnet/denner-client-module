5697224ab1caf958514caab1e4d457f45c2b7183
Ivan Wolf <iwolf@detailag.ch>
2018-11-28 15:10:38 +0100

    Tweaked configuration of client

---
+++ tests/Factory/FactoryTestCase.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DennerTest\Client\Factory;
+
+use PHPUnit\Framework\TestCase;
+
+use Prophecy\Prophecy\ObjectProphecy;
+
+use Interop\Container\ContainerInterface;
+
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+abstract class FactoryTestCase extends TestCase
+{
+    /**
+     * @var ObjectProphecy
+     */
+    private $services;
+
+    /**
+     * @var FactoryInterface
+     */
+    private $factory;
+
+    protected function setUp()
+    {
+        $this->services = $this->prophesize(ContainerInterface::CLASS);
+        $this->factory = $this->createFactory();
+    }
+
+    protected function getServices(): ObjectProphecy
+    {
+        return $this->services;
+    }
+
+    protected function getFactory(): FactoryInterface
+    {
+        return $this->factory;
+    }
+
+    abstract protected function createFactory(): FactoryInterface;
+}
